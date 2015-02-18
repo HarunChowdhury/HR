94f4143bb17e6edd9299ebb1d40f47052790f229
harunchowdhury <trishamomo1@gmail.com>
2015-02-18 19:36:55 +0600

    Success

---
+++ SchoolManagement/SchoolManagement.Manager/StudentManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SchoolManagement.Model;
+
+namespace SchoolManagement.Manager
+{
+   public class StudentManager
+   {
+       private SchoolManagementDbEntities Db;
+       public StudentManager()
+       {
+           Db = new SchoolManagementDbEntities();
+       }
+
+       public void Add(Student aStudent)
+       {
+           //GET STUDENT from database
+
+           DbSet<Student> students = Db.Students;
+
+           students.Add(aStudent);
+
+           Db.SaveChanges();
+       }
+
+
+       public void Delete(int id)
+       {
+           /*   Student s =(from student in Db.Students
+           where student.Id == id
+           select student).FirstOrDefault(); */
+         // Student s =(Db.Students.Where(student => student.Id == id)).FirstOrDefault();
+           Student s = Db.Students.FirstOrDefault(student => student.Id == id);
+
+       }
+
+       public List<Student> GetStudent()
+       {
+           return Db.Students.ToList();
+       }
+
+   }
+}
---
+++ SchoolManagement/SchoolManagementConsoleApplication/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SchoolManagement.Manager;
+using SchoolManagement.Model;
+
+namespace SchoolManagementConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Student student = new Student() { Name = "Harun", Address = "Rowmari", Phone = "0124654" };
+            //Student student1 = new Student() { Name = "Kamal", Address = "Rajibpur", Phone = "2654984" };
+
+            StudentManager manager = new StudentManager();
+
+            List<Student> students=manager.GetStudent();
+             //manager.Add(student);
+             //manager.Add(student1);
+            foreach (var aStudent in students)
+            {
+                Console.WriteLine("Name :"+aStudent.Name+" "+ aStudent.Address); 
+            }
+
+           // Console.WriteLine("Save Data Successfull");
+            Console.ReadKey();
+
+        }
+    }
+}
