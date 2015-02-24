e801d7eccb6b1dcbffc406720ae6349cb8f1ec73
harunchowdhury <trishamomo1@gmail.com>
2015-02-24 10:55:19 -0800

    V001

--- SchoolManagement/SchoolManagementConsoleApplication/Program.cs
+++ SchoolManagement/SchoolManagementConsoleApplication/Program.cs
@@ -12,17 +12,29 @@ namespace SchoolManagementConsoleApplication
     {
         static void Main(string[] args)
         {
-            //Student student = new Student() { Name = "Harun", Address = "Rowmari", Phone = "0124654" };
-            //Student student1 = new Student() { Name = "Kamal", Address = "Rajibpur", Phone = "2654984" };
+            Student student = new Student() { Name = "Harun", Address = "Rowmari", Phone = "0124654" };
+            Student student1 = new Student() { Name = "Kamal", Address = "Rajibpur", Phone = "2654984" };
 
             StudentManager manager = new StudentManager();
 
             List<Student> students=manager.GetStudent();
-             //manager.Add(student);
-             //manager.Add(student1);
+            manager.Add(student);
+            manager.Add(student1);
             foreach (var aStudent in students)
             {
                 Console.WriteLine("Name :"+aStudent.Name+" "+ aStudent.Address); 
+            }
+
+            {
+                Console.WriteLine("Name :" + aStudent.Name + " " + aStudent.Address);
+            }
+
+            {
+                Console.WriteLine("Name :" + aStudent.Name + " " + aStudent.Address);
+            }
+
+            {
+                Console.WriteLine("Name :" + aStudent.Name + " " + aStudent.Address);
             }
 
            // Console.WriteLine("Save Data Successfull");
